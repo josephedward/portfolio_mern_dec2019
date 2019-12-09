703b7b1bd7662313b402171debcd3f3e92b11a4c
Joe <josephedwardwork@gmail.com>
2019-12-08 23:41:50 -0500

    updates

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -11,7 +11,7 @@ function Nav() {
     <hr/>
       <a style={{color:"black",margin:"10px"}} href="/projects">Projects </a>
       <a style={{color:"black",margin:"10px"}} href="/terminal">Terminal </a>
-      <a style={{color:"black",margin:"10px"}} href="/jq_porfolio">jQuery 'Concept' Portfolio<span role="img" aria-label="smile emoji"> 😁 </span></a>
+      <a style={{color:"black",margin:"10px"}} href="https://josephedward.github.io/portfolio-site/">jQuery 'Concept' Portfolio<span role="img" aria-label="smile emoji"> 😁 </span></a>
     </nav>
   );
 }
