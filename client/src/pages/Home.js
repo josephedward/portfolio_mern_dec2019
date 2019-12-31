12721469474ad02be9ca6bbe84a506011bdeb7a5
Joe <josephedwardwork@gmail.com>
2019-12-31 01:25:55 -0500

    commit

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -6,7 +6,7 @@ class Home extends Component{
     render(){
         return(
             <div >
-                <iframe  width="100%" height="700" title="Joe's Old Site" src="https://josephedward.github.io/portfolio-site/"/>
+                <iframe  width="100%" height="700" title="Joe's Old Site" src="https://josephedward.github.io/jq_personal/"/>
             </div>
         );
     }
