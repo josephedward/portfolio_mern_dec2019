6fed6f2d73e80538a153892db1beb0d669740f2f
Joe <josephedwardwork@gmail.com>
2020-02-11 16:03:30 -0500

    commit

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,10 +1,8 @@
 import React, { Component } from "react";
 import { Menu } from "semantic-ui-react";
-// import testImage from "./matrix.jpeg";
 
 class Nav extends Component {
   render() {
-    // const { activeItem } = this.state
 
     return (
       <Menu className="navbar navbar-expand-lg navbar-dark bg-danger" 
@@ -28,11 +26,7 @@ class Nav extends Component {
         <Menu.Item style={{ ...txtStyle2 }} href="/terminal">
           Terminal{" "}
         </Menu.Item>
-        <Menu.Item style={{ ...txtStyle2 }}>
-          {
-            // href="https://jeffhuang.com/designed_to_last/"
-            /* Need the to create plain HTML site */
-          }
+        <Menu.Item style={{ ...txtStyle2 }} href="/persistent.html">
           Persistent{" "}
         </Menu.Item>
       </Menu>
@@ -43,32 +37,11 @@ class Nav extends Component {
 export default Nav;
 
 const navStyle = {
-  //  "background-image": `url('${testImage}')`,
-
-  // "background-color": "red",
-  // position:"fixed",
-  // width:"100%",
-  // zIndex:"10"
     marginBottom:"0px",
     color: "white"
 };
 
-// const txtStyle = {
-//   color: "white",
-//   margin: "10px"
-// };
 
 const txtStyle2 = {
   color: "white",
-  // "-webkit-text-stroke-width": "1px",
-  //   "-webkit-text-stroke-color": "black",
-  // fontSize: "20px", 
-  // fontOutline:"red solid 3px",
-
-  // "text-shadow": "black 0px 0px 1px",
-  // "-webkit-font-smoothing": "antialiased",
-  // border: "3px solid black",
-  // fontFamily:"helvetica",
-  // margin: "10px",
-  // backgroundColor: "white"
 };
