72d1c33fb3157252c516ccd413c7f04446ed7936
Joe <josephedwardwork@gmail.com>
2020-09-17 01:53:20 -0400

    commit

---
+++ client/src/pages/liriBot/keys.js
@@ -0,0 +1,10 @@
+console.log('keys.js loaded');
+
+exports.spotify = {
+  id:"8654c053459b48f5af5f8af5c1874bf5", 
+  // process.env.SPOTIFY_ID,
+  secret: "9dcaacc693e5468eb8edb13e1bd3308f" 
+  // process.env.SPOTIFY_SECRET
+};
+
+exports.OMDB_KEY= "b3c0b435"
