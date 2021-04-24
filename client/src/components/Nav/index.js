baaa9e9bb0abf22c2dd2ea78c7bb1301a4b78404
Joe <josephedwardwork@gmail.com>
2021-04-23 23:08:44 -0400

    Update index.js

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
-import { Menu } from "semantic-ui-react";
+import { Menu, Button } from "semantic-ui-react";
 import Toggle from "react-toggle";
 import "./index.css";
 
 require("typeface-aleo");
 require("typeface-open-sans");
+
+// const [, updateState] = React.useState();
+// const forceUpdate = React.useCallback(() => updateState({}), []);
 
 function getRandomColor(){
   var randomColor = Math.floor(Math.random() * 16777215).toString(16);
@@ -13,6 +16,13 @@ return randomColor
 
 
 class Nav extends Component {
+
+
+  handleClick = () => {
+    // force a re-render
+    this.forceUpdate();
+  };
+
   render() {
     return (
       <Menu style={{ ...navStyle }} stackable>
@@ -50,6 +60,17 @@ class Nav extends Component {
             }}
           />
         </Menu.Item>
+        {/* <Menu.Item style={{ ...txtStyle2 }}>
+          <button
+            onClick={
+              () => {
+               document.location.reload()
+            }
+            }
+          >
+        Change Colors
+          </button>
+        </Menu.Item> */}
       </Menu>
     );
   }
@@ -60,23 +81,26 @@ export default Nav;
 const navStyle = {
   marginBottom: "0px",
   // color: "aqua",
-  border: "10px solid aqua",
+  border: `10px solid #${getRandomColor()}`,
   // background: "rgba(0,0,0,0)",
   backgroundColor: `#${getRandomColor()}`,
   width: "100%",
   display: "flex",
   justifyContent: "center",
-  // outline:"15px solid orange"
+
 };
 
 const txtStyle2 = {
   fontFamily: "Helvetica",
 
   "-webkit-text-stroke-width": "1px",
-  "-webkit-text-stroke-color": "white",
+  "-webkit-text-stroke-color": 
+  // "black",
+  `#${getRandomColor()}`,
   backgroundColor: `#${getRandomColor()}`,
   fontSize: "20px",
   fontWeight: "bold",
-  color: "darkblue",
-  border: "5px solid darkblue",
+  color: "black",
+  // `#${getRandomColor()}`,
+  border: `3px solid #${getRandomColor()}`,
 };
