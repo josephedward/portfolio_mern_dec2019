ddaef2975f85e7eb56511ee8698a8a1da651371d
josephedward <josephedwardwork@gmail.com>
2021-05-20 11:57:00 -0400

    cleanup

--- server.js
+++ server.js
@@ -8,39 +8,23 @@ const PORT = process.env.PORT || 8888;
 // const gapi = require("./GHAPI.js");
 const path = require('path')
 require('dotenv').config({ path:"../.env" });
-  // path.resolve(__dirname, '../.env') })
 
 
-// Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// if (process.env.NODE_ENV !== 'production') {
- 
-// }
-
-// Add routes, both API and view
-app.use(routes);
-//libridex uses :
-// Add routes, both API and view
 app.use("/",routes);
 
-// app.use("/creds", function(req,res){
-//   res.send(process.env)
-// })
-
-// Connect to the Mongo DB
-//added line to avoid deprecation warning
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/react_project_list",
   { useNewUrlParser: true, useUnifiedTopology: true }
 );
 
-// Start the API server
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server NOW listening on PORT ${PORT}!`);
 });
